ee9db83934b71c79af7b6cc335f540081330123f
Paras Kanwar <paraskanwar@Mac-2.modem>
2025-07-18 10:30:52 +1000

    feat: added qa env

---
+++ environments/non-prod/qa/main.tf
@@ -0,0 +1,27 @@
+provider "google" {
+  project = var.project_id
+  region  = "australia-southeast1"
+}
+
+module "cloudsql" {
+  source           = "git::https://github.com/paraskanwarit/terraform-modules.git//cloudsql?ref=v1.0.0-beta"
+  instance_name    = "qa-sql-instance"
+  database_version = "POSTGRES_16"
+  region           = "australia-southeast1"
+  tier             = "qa-perf-optimized-N-2"
+  availability_type = "REGIONAL"
+  disk_size        = 50
+  disk_type        = "PD_SSD"
+  ipv4_enabled     = false
+  private_network  = var.private_network
+  authorized_networks = []
+  root_password    = var.sql_root_password
+  db_user          = "qauser"
+  db_password      = var.sql_app_password
+  db_name          = "qadb"
+  labels = {
+    environment = "qa"
+    owner       = "team"
+  }
+}
+#test 1
